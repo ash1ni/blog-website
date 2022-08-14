801916be42632fbb4ae5c14c05f865afc3757f3d
ash1ni <itsashwani001@gmail.com>
2022-08-14 23:52:26 +0530

    connected app with database - performed create and update methods

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const url = process.env.Blog_Url
+mongoose.connect(url,{useNewUrlParser: true})
+const connection = mongoose.connection
+connection.once('open', ()=>{
+  console.log('connected successfully')
+})
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,10 +28,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+const postSchema = {
+  title: String,
+  content: String
+}
+
+const Post = mongoose.model("Post", postSchema)
+
+
 app.get("/",(req,res)=>{
- res.render("home",{
+ Post.find({}, (err, posts)=>{
+  if(err){
+    console.log(err)
+  }else{
+    res.render("home",{
          startingContent: homeStartingContent,
          posts:posts
+ })
+  }
  })
  
 
@@ -49,13 +73,18 @@ app.get("/compose",(req,res)=>{
   //res.send()
 })
 app.post('/compose',(req,res)=>{
-  const post = {
+  const post = new Post( {
     title: req.body.composeText,
     content:req.body.composePost
-  }
+  })
   posts.push(post)
+  post.save((err)=>{
+    if(!err){
+      res.redirect("/")
+    }
+  })
   
-  res.redirect("/")
+  
 
 })
 app.get("/posts/:postName", (req,res)=>{
